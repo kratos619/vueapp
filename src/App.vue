36f9fa490b816e77099e608d41daf40c0b647705
kratos619 <gaurav.palaspagar@hotmail.com>
2019-03-02 22:12:30 +0530

    Mixin and Filters

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="p-4 col-md-6 mx-auto">
+          <h1>Mixin And Filters</h1>
+          <p>{{text|toUppercase}}</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      text: "jaskjdhakj"
+    };
+  },
+  filters: {
+    toUppercase(value) {
+      return value.toUpperCase();
+    }
+  }
+};
 </script>
 
 <style>
