921f3272505bc92edd8dd08242d498b5b58ff90c
kratos619 <gaurav.palaspagar@hotmail.com>
2019-03-02 13:14:27 +0530

    Initial commit

---
+++ src/components/HeaderProgreebar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 mx-auto">
+          <div class="progress">
+            <div
+              class="progress-bar bg-primary"
+              role="progressbar"
+              :style="{width:progressBarWidth + '%'}"
+            >{{quoteCount}} / {{maxQuotes}}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["quoteCount", "maxQuotes"],
+
+  data() {
+    return {
+      progressBarWidth: 0
+    };
+  },
+  updated() {
+    this.progressBarWidth =
+      (this.$props.quoteCount / this.$props.maxQuotes) * 100;
+    console.log(this.progressBarWidth);
+  }
+};
+</script>
+<style scope>
+</style>
---
+++ src/components/New.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>this is New</h1>
+    <button v-on:click="counter++">Click Me</button>
+    <p>{{counter}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
