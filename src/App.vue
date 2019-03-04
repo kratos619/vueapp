0a9e819edef00e06f70a56286b2ed263c633498b
kratos619 <gaurav.palaspagar@hotmail.com>
2019-03-04 14:28:47 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
   <div id="app">
-    
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 mx-auto">
+          <h1>hello</h1>
+          <button v-on:click="show = !show" class="btn btn-primary">Click Me</button>
+          <transition>
+            <p v-if="show" class="alert alert-info">hi</p>
+          </transition>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  
-}
+  data() {
+    return {
+      show: false
+    };
+  },
+  methods: {}
+};
 </script>
-<style>
+<style scoped>
 </style>
