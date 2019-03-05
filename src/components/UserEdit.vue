2f3826a7d6f5bc549c5e6ceecf1afe91819b4eb2
kratos619 <gaurav.palaspagar@hotmail.com>
2019-03-05 14:16:34 +0530

    passing Query parameters ?local=asdasd&q=sdasd

--- src/components/UserEdit.vue
+++ src/components/UserEdit.vue
@@ -3,6 +3,8 @@
     <h1>Users Edite</h1>
     <p>{{id}}</p>
     <p>{{name}}</p>
+    <p>{{local}}</p>
+    <p>{{q}}</p>
     <router-link to="/">back</router-link>
     <router-link to="/useredit/1/saurav">Link to data</router-link>
     <router-link to="/useredit/3/sdasd">Link to data 2</router-link>
@@ -15,7 +17,9 @@ export default {
     return {
       // read pearameters
       id: this.$route.params.id,
-      name: this.$route.params.name
+      name: this.$route.params.name,
+      local: this.$route.query.local,
+      q: this.$route.query.q
     };
   },
   watch: {
