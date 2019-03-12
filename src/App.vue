9f5a70405bf2c96408f8ad4d0868c1b81ee638ba
kratos619 <gaurav.palaspagar@hotmail.com>
2019-03-12 11:51:39 +0530

    code cleaning

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,15 @@
 <template>
   <div id="app">
-    <div class="container">
-      <header></header>
-      <div class="row">
-        <div class="col-md-6">
-          <Candidate></Candidate>
-        </div>
-        <div class="col-md-6">
-          <Employee></Employee>
-        </div>
-      </div>
-    </div>
+    <Header></Header>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
-import Candidate from "./components/Candidate.vue";
-import Employee from "./components/Employee.vue";
 export default {
   name: "App",
   components: {
-    Header,
-    Candidate,
-    Employee
+    Header
   }
 };
 </script>
