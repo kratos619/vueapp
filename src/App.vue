f07e0f5e805a3b63b02b37625d6b050fe5cd681b
kratos619 <gaurav.palaspagar@hotmail.com>
2019-03-25 10:37:28 +0530

    code setup

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
-  
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <h1>Routing</h1>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
-export default {
-
-};
+export default {};
 </script>
 
 <style>
