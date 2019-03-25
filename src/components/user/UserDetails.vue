f434e559f204707979e8641a6485a49be0822a67
kratos619 <gaurav.palaspagar@hotmail.com>
2019-03-25 13:48:28 +0530

    Making routes more dynamic

--- src/components/user/UserDetails.vue
+++ src/components/user/UserDetails.vue
@@ -2,6 +2,7 @@
   <div>
     <h3>Some User Details</h3>
     <p>User Loaded Has ID : {{id}}</p>
+    <router-link tag="button" v-bind:to="id + '/edit'">Edit User</router-link>
   </div>
 </template>
 
--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -1,4 +1,6 @@
-<template></template>
+<template>
+  <div>edit</div>
+</template>
 
 <script>
 export default {};
